3b3007ea47a9e4189ed60bb6518ace96db3382bd
Kummara-Rajasekhar <kummararajasekhar17092004@gmail.com>
2024-06-12 13:15:52 +0900

    second commit

--- 1prjt/javascript.js
+++ 1prjt/javascript.js
@@ -4,9 +4,24 @@ menu.onclick=()=>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+let section=document.querySelector('.section');
+let navLinks=document.querySelector('header .navber a');
 window.onscroll=()=>{
+
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    section.foreach(sec=>{
+      let top=window.scrollY;
+      let height=sec.offsetHeight;
+      let offset=sec.offsetTop-150;
+      let id=sec.getAttribute('id');
+      if(top=>offset && top<offset+height){
+        navLinks.forEach(links=>{
+          links.classList.remove('active');
+           document.querySelector('header .navber a[href*='+id+']').classList.add('active')
+        })
+      }
+    })
 }
 document.querySelector('#search-icon').onclick=()=>{
     document.querySelector('#search-form').classList.toggle('active');
@@ -14,7 +29,7 @@ document.querySelector('#search-icon').onclick=()=>{
 document.querySelector('#close').onclick=()=>{
     document.querySelector('#search-form').classList.remove('active');
 }
-var swiper = new Swiper(".swiper", {
+var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -27,3 +42,39 @@ var swiper = new Swiper(".swiper", {
     },
     loop:true,
   });
+  /* var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+ 
+    loop:true,
+    breakpoints:{
+      0:{
+        slidesPerView:1,
+
+      },
+      640:{
+        slidesPerView:2,
+
+      },
+      768:{
+        slidesPerView:2,
+
+      },
+      1024:{
+        slidesPerView:3,
+
+      },
+    },
+  }); */
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+
+  }
+  function FadeOut(){
+    setInterval(loader,3000);
+  }
+  window.onload=FadeOut;
